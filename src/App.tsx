45e4909c00b6dc4ebdd9fd9ba879ffaa2a908c25
laurence ede <laurence.ede@gmail.com>
2022-04-10 20:07:48 +0100

    Add firebase & google auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
+import { useEffect, useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import Account from "@mui/icons-material/AccountCircle";
 import { Paper } from "@mui/material";
+import { onAuthStateChanged, signInWithRedirect, signOut } from "firebase/auth";
+
+import { auth, provider } from "./config";
 
 export default function App() {
+  const [authenticated, setAuthenticated] = useState<boolean>();
+
+  useEffect(() => {
+    const authStartup = async () => {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setAuthenticated(true);
+        } else {
+          setAuthenticated(false);
+        }
+      });
+    };
+
+    authStartup();
+  }, []);
+
+  const login = () => signInWithRedirect(auth, provider);
+  const logout = () => signOut(auth);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -13,7 +37,16 @@ export default function App() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Archery
           </Typography>
-          <Button color="inherit">Login</Button>
+          {authenticated === true && (
+            <Button onClick={logout} color="inherit">
+              <Account />
+            </Button>
+          )}
+          {authenticated === false && (
+            <Button onClick={login} color="inherit">
+              Login
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
       <Box
---
+++ src/config/firebase.ts
@@ -0,0 +1,16 @@
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCq8scRYr-rCJnpUiGRP03KIQK74aIhvyo",
+  authDomain: "archery-89374.firebaseapp.com",
+  projectId: "archery-89374",
+  storageBucket: "archery-89374.appspot.com",
+  messagingSenderId: "742659309021",
+  appId: "1:742659309021:web:092cac756842a458e98a34",
+  measurementId: "G-252PD6QKNZ",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const provider = new GoogleAuthProvider();
+export const auth = getAuth();
