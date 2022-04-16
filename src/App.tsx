699e699200ad21ac7d7c05624950f36a85639473
laurence ede <laurence.ede@gmail.com>
2022-04-16 08:34:17 +0100

    Add redirect from / to sessions

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Box from "@mui/material/Box";
 import { Paper } from "@mui/material";
 
 import { AppBar } from "./components";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { ActiveSession, NewSession, Sessions } from "./views";
 
 export default function App() {
@@ -25,6 +25,7 @@ export default function App() {
                 <Route path="new" element={<NewSession />} />
                 <Route path=":sessionId/active" element={<ActiveSession />} />
               </Route>
+              <Route path="/" element={<Navigate replace to="/sessions" />} />
             </Routes>
           </Paper>
         </Box>
