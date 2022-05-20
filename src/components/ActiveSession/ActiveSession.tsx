9ab9dcfa3dbd2278e6146f506756d66e609406dd
Laurence Ede <laurence.ede@gmail.com>
2022-05-20 22:15:42 +0100

    Add TODO about splitting ActiveSession

--- src/components/ActiveSession/ActiveSession.tsx
+++ src/components/ActiveSession/ActiveSession.tsx
@@ -3,6 +3,11 @@ import { useEffect, useReducer } from "react";
 import { NewSessionSelection } from "./NewSessionSelection";
 import { reducer, initialState, Stage } from "./reducer";
 
+// TODO: Split into `NewSession` on `/sessions/new` and `ActiveSession` on `/sessions/active`
+// This will allow the back button to work more intuitively.
+// Could use query params to initialise active session. Alternatively could look at Zustand.
+// On `/new` can display warning if there is already an active session.
+
 export function ActiveSession() {
   const [{ stage }, dispatch] = useReducer(reducer, initialState);
 
